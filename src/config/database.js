c01d069fbe5128f6aa5466fe0770e50a01bb8967
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-09-17 10:46:20 -0500

    [+] Mongo: Conexion (Mongoose), .env Establece Variables de Entorno

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require( 'mongoose' ),             // Importa paquete 'mongoose' (ORM) para MongoDB
+      URI = process .env .MONGODB_URI               // 'process': Objeto global de Node
+        ? process .env .MONGODB_URI                 //    En el root del proyecto se debe crear el archivo .env con las variables de entorno deseadas (No debe agregarse al repositorio por seguridad)
+        : 'mongodb://localhost/agencia-viajes-test';      
+
+console .log( 'URI', URI );
+
+// Conexión a una instancia o servidor de MongoDB
+mongoose .connect( URI, {
+    useCreateIndex: true,       // Falso por defecto. Configurado para truehacer que la compilación de índice predeterminada de Mongoose se use en createIndex()
+    useNewUrlParser: true,      // Bandera para permitir a los usuarios recurrir al antiguo analizador (Controlador DB MongoDB)
+    useFindAndModify: false,    // Configurado para false hacer findOneAndUpdate() y findOneAndRemove() usar nativo en findOneAndUpdate() lugar de findAndModify()
+    useUnifiedTopology: true    // Configura nuevo motor de topología de Mongoose
+});
+
+const connection = mongoose .connection;
+
+// Valida si la conexión se abre
+connection .once( 'open', () =>  {
+    console .log( `Base de datos conectada` );
+});
